d0367f9229d3673866c927376ad05275128dea3e
Elorze <2270938474@qq.com>
2025-11-26 20:26:04 +0800

    4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   ui: {
     fonts: false,
   },
-  modules: ["@nuxt/icon", "@nuxt/ui", "@pinia/nuxt","@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/icon", "@nuxt/ui", "@pinia/nuxt"],
   css: ["~/assets/css/main.css"],
   routeRules: {
     "/api/**": {
